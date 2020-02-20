a551e13496e695d6f5c436de586fcf9324595627
Chukwujama Osinachi Victor <vicraph7@gmail.com>
2020-02-20 23:30:17 +0100

    Add features to SEO component up to helmet

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,11 +1,65 @@
 import React from 'react';
+import { Helmet } from "react-helmet"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
 
-const SEO = () => {
-   return (
-    <div>
-      
-    </div>
+const SEO = ({ title, description, image, pathname, article }) => {
+  const data = useStaticQuery(graphql`
+    query SEO {
+      site {
+        siteMetadata {
+          defaultTitle: title
+          defaultDescription: description
+          siteUrl: url
+        }
+      }
+    }
+  `);
+  for (let i in data) {
+    if (data[i] === undefined || data[i] === null) {
+      data[i] = ''
+    }
+  }
+  return (
+    <Helmet title={seo.title} titleTemplate={titleTemplate}>
+      <meta name="description" content={seo.description} />
+      <meta name="image" content={seo.image} />
+      {seo.url && <meta property="og:url" content={seo.url} />}
+      {(article ? true : null) && (
+        <meta property="og:type" content="article" />
+      )}
+      {seo.title && <meta property="og:title" content={seo.title} />}
+      {seo.description && (
+        <meta property="og:description" content={seo.description} />
+      )}
+      {seo.image && <meta property="og:image" content={seo.image} />}
+      <meta name="twitter:card" content="summary_large_image" />
+      {twitterUsername && (
+        <meta name="twitter:creator" content={twitterUsername} />
+      )}
+      {seo.title && <meta name="twitter:title" content={seo.title} />}
+      {seo.description && (
+        <meta name="twitter:description" content={seo.description} />
+      )}
+      {seo.image && <meta name="twitter:image" content={seo.image} />}
+    </Helmet>
   )
 }
 
-export default SEO;
+export default SEO
+
+SEO.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  image: PropTypes.string,
+  pathname: PropTypes.string,
+  article: PropTypes.bool,
+}
+
+SEO.defaultProps = {
+  title: null,
+  description: null,
+  image: null,
+  pathname: null,
+  article: false,
+}
