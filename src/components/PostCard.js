f8d389d74e058d369fc05d07480801176bf2ca54
Chukwujama Osinachi Victor <vicraph7@gmail.com>
2020-02-27 22:49:32 +0100

    Trying stuff

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -22,16 +22,24 @@ const PostCard = (props) => {
           }
           .post-card-image {
             max-height: 200px !important;
+            // height:inherit !important;
+          }
+          .card-img-cont {
+            height:200px;
           }
         `}
       </style>
       <Link to={props.path || "/404"} className="post-link">
         <Card>
-          {
-            props.image ?
-              <CardImg className = "post-card-image" key = {new Date()} top width="100%" src={props.image} alt="Stuff" />:
-              ''
-          }
+          {/* <div className="card-img-cont"> */}
+
+
+            {
+              props.image ?
+                <CardImg className="post-card-image" key={new Date()} top width="100%" src={props.image} alt="Stuff" /> :
+                ''
+            }
+          {/* </div> */}
           <CardBody>
             <CardTitle className="techwinx-card-title">{props.title} </CardTitle>
             <CardText>{props.content} </CardText>
